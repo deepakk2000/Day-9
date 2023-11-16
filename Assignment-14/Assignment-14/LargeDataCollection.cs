3073eddd724c971fa78c70ea8375efaa9f840864
deepakk2000 <deepakjc112000@gmal.com>
2023-11-16 16:41:48 +0530

    A-14

---
+++ Assignment-14/Assignment-14/LargeDataCollection.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_14
+{
+    internal class LargeDataCollection:IDisposable
+    {
+        private List<object> dataCollection;
+
+        public LargeDataCollection(IEnumerable<object> initialData)
+        {
+            dataCollection = new List<object>(initialData);
+        }
+
+        public void AddElement(object element)
+        {
+            dataCollection.Add(element);
+        }
+
+        public void RemoveElement(object element)
+        {
+            dataCollection.Remove(element);
+        }
+
+        public object AccessElement(int index)
+        {
+            if (index >= 0 && index < dataCollection.Count)
+                return dataCollection[index];
+            else
+                return null; // or throw an exception based on your requirements
+        }
+
+        public void Dispose()
+        {
+            // Release any unmanaged resources here
+            // Example: Close connections, file handles, etc.
+
+            // Set the internal data structure to null
+            dataCollection = null;
+
+            // Optionally, suppress finalization if not needed
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Assignment-14/Assignment-14/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an instance of LargeDataCollection
+            using (var myCollection = new LargeDataCollection(new List<object> { 1, "Hello", new CustomObject() }))
+            {
+                // Demonstrate adding, removing, and accessing elements
+                myCollection.AddElement(42);
+                myCollection.RemoveElement("Hello");
+                Console.WriteLine(myCollection.AccessElement(0));
+
+                // Explicitly call Dispose to release resources
+                myCollection.Dispose();
+            }
+        }
+    }
+
+    class CustomObject
+
+        }
+    }
+}
